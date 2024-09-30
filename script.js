a30c034dc38f4f856a125678e54e262bee466779
Tushar Pandey <tp867912@gmail.com>
2024-10-01 00:58:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ $(document).ready(function(){
     $(window).on('scroll load',function(){
         $("menu").removeClass('fa-times');
         $('header').removeClass('toggle');
+
+        
+    });
+
+    $('a[href*="#"]').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+
+            scrollTop : $($(this).attr('href')).offset().top,
+
+        },
+        500,
+        'linear'
+    );
+
     });
 
 });
